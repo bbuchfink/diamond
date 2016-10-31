85e1a77eb834173586c815f4a1005ada606a1d3b
bbuchfink <buchfink@gmail.com>
2016-10-31 14:13:15 +0100

    fixed help to include btop

--- src/basic/config.cpp
+++ src/basic/config.cpp
@@ -81,6 +81,7 @@ Config::Config(int argc, const char **argv)
 \tgaps means Total number of gaps\n\
 \tppos means Percentage of positive - scoring matches\n\
 \tqframe means Query frame\n\
+\tbtop means Blast traceback operations(BTOP)\n\
 \tstitle means Subject Title\n\
 \tsalltitles means All Subject Title(s), separated by a '<>'\n\
 \tqcovhsp means Query Coverage Per HSP\n\
