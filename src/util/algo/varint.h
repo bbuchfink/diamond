089a2382b2bbba06d3bb8e813ab2edb84a02c14f
bbuchfink <buchfink@gmail.com>
2018-03-04 14:10:29 +0100

    fixed include

--- src/util/algo/varint.h
+++ src/util/algo/varint.h
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VARINT_H_
 
 #include <stdint.h>
+#include <stdexcept>
 #include "../intrin.h"
 
 template<typename _out>
