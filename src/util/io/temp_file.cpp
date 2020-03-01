56d0cab60eb1181b9b48601357960d0afc4f6e51
Benjamin Buchfink <buchfink@gmail.com>
2020-03-01 16:25:55 +0100

    Fixed call.

--- src/util/io/temp_file.cpp
+++ src/util/io/temp_file.cpp
@@ -73,7 +73,7 @@ pair<string, int> TempFile::init(bool unlink)
 	if (config.no_unlink || !unlink)
 		unlinked = false;
 	else
-		unlinked = (unlink(s) >= 0);
+		unlinked = (::unlink(s) >= 0);
 	return std::make_pair(string(s), fd);
 #endif
 }
