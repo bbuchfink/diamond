d49c9a4f0318f4c7bb22df311a89c33f1b544249
Patrick Ettenhuber <pettenhuber@gmail.com>
2020-04-25 11:01:39 +0200

    adding headers and descriptions

--- src/cluster/cluster_registry.h
+++ src/cluster/cluster_registry.h
@@ -1,3 +1,21 @@
+/****
+DIAMOND protein aligner
+Copyright (C) 2013-2018 Benjamin Buchfink <buchfink@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+****/
+
 #pragma once
 #include "cluster.h"
 #include "mcl.h"
@@ -9,6 +27,7 @@ namespace Workflow { namespace Cluster{
 class ClusterRegistry{
 private:
 	ClusterRegistry(){};
+	// To include new clustering algorithms add the instantiation here and in the cluster_registry.cpp file. Then add it to the StaticConstructor below
 	static MCL mcl;
 	static MultiStep multiStep;
 public:
@@ -34,6 +53,7 @@ public:
 	}
 	static struct StaticConstructor {
 		StaticConstructor() {
+			// Add any new clustering algorithm here
 			regMap.emplace(multiStep.get_key(), &multiStep);
 			regMap.emplace(mcl.get_key(), &mcl);
 		}
--- src/cluster/mcl.cpp
+++ src/cluster/mcl.cpp
@@ -1,3 +1,21 @@
+/****
+DIAMOND protein aligner
+Copyright (C) 2013-2018 Benjamin Buchfink <buchfink@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+****/
+
 #include "mcl.h"
 
 using namespace std;
