5f2ab15b995cf76e6adba5eee0b4f40d7aa4b72c
Patrick Ettenhuber <pettenhuber@gmail.com>
2020-05-22 07:46:46 +0200

    update copyright

--- src/cluster/disjoint_set.h
+++ src/cluster/disjoint_set.h
@@ -1,6 +1,7 @@
 /****
 DIAMOND protein aligner - Markov Clustering Module
-Copyright (C) 2019 Patrick Ettenhuber <pettenhuber@gmail.com>
+Copyright (C) 2020 QIAGEN A/S (Aarhus, Denmark)
+Code developed by Patrick Ettenhuber <patrick.ettenhuber@qiagen.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
