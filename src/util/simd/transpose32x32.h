8ff80ad9b58c72436383bad1c18daf0ba668707c
Benjamin Buchfink <buchfink@gmail.com>
2020-05-30 15:13:31 +0200

    Macos error.

--- src/util/simd/transpose32x32.h
+++ src/util/simd/transpose32x32.h
@@ -29,6 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 static inline void transpose(const signed char** data, size_t n, signed char* out, const __m256i&) {
 	__m256i r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, t;
+	r0 = r1 = r2 = r3 = r4 = r5 = r6 = r7 = r8 = r9 = r10 = r11 = r12 = r13 = r14 = r15 = r16 = r17 = r18 = r19 = r20 = r21 = r22 = r23 = r24 = r25 = r26 = r27 = r28 = r29 = r30 = r31 = _mm256_setzero_si256();
 
 	switch (n) {
 	case 32: r0 = _mm256_loadu_si256((const __m256i*) * (data++));
