3e0d1524428b20cad3e350df74acc9370c8be308
Benjamin Buchfink <buchfink@gmail.com>
2020-06-01 12:58:58 +0200

    Fixed tests.

--- src/test/test_cases.cpp
+++ src/test/test_cases.cpp
@@ -46,8 +46,8 @@ const uint64_t ref_hashes[] = {
 0xa2ea51fd070b9485,
 0x7c90bb1c62c71cfb,
 0xc40f9bffc00c4f97,
-0x13a8f9621dd17c77,
-0xdf32d46bc18e400a
+0x494651aafd055246,
+0xdf32d46bc18e400a,
 };
 
 }
