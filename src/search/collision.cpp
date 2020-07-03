8f5f8b24a7cabb3a7a5d26f033d3ec404f962073
Joseph Mingrone <jrm@ftfl.ca>
2020-07-02 22:20:43 -0300

    Fix build on FreeBSD by removing assert referencing config.window

--- src/search/collision.cpp
+++ src/search/collision.cpp
@@ -92,7 +92,6 @@ bool is_primary_hit(const Letter *query,
 	const unsigned sid,
 	const unsigned len)
 {
-	assert(len > 0 && len <= config.window * 2);
 	const bool chunked(config.lowmem > 1);
 	uint64_t mask = reduced_match32(query, subject, len);
 	unsigned i = 0;
