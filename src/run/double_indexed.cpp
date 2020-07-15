3fa545bf8501f267a342043f35e0c69361f3ad51
Klaus Reuter <khr@mpcdf.mpg.de>
2020-07-15 14:22:01 +0200

    [wip] implementing failsafe mode

--- src/run/double_indexed.cpp
+++ src/run/double_indexed.cpp
@@ -407,6 +407,10 @@ void master_thread(DatabaseFile *db_file, task_timer &total_timer, Metadata &met
 		for (size_t i=0; i<current_query_chunk; ++i) {
 			const string annotation = "# query_chunk=" + std::to_string(i);
 			db_file->save_partition(get_ref_part_file_name(stack_align_todo, i), annotation);
+			auto tag = get_ref_part_file_name(stack_join_todo, i);
+			P->create_stack(tag);
+			P->get_stack(tag)->push("TOKEN");
+			P->delete_stack(tag);
 		}
 
 		return;
