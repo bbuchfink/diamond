08ef5f8afbe7226355afcb832b66e96905580737
Benjamin Buchfink <buchfink@gmail.com>
2020-10-13 15:16:41 +0200

    Handle ev 0.

--- src/tools/roc.cpp
+++ src/tools/roc.cpp
@@ -108,6 +108,8 @@ struct Histogram {
 	}
 
 	static int bin(double evalue) {
+		if (evalue == 0.0)
+			return 0;
 		int bin = std::round(std::log2(evalue)) - DBL_MIN_EXP;
 		if (bin < 0 || bin >= BINS)
 			throw std::runtime_error("Evalue exceeds binning range.");
