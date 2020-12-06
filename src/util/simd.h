e517882749f57a29780e79cb84d3e390ddcd4959
Benjamin Buchfink <buchfink@gmail.com>
2020-12-06 13:08:09 +0100

    Fixed namespaces.

--- src/util/simd.h
+++ src/util/simd.h
@@ -49,6 +49,7 @@ enum Flags { SSSE3 = 1, POPCNT = 2, SSE4_1 = 4, AVX2 = 8 };
 Arch arch();
 
 #if defined(RUNTIME_DISPATCH) && defined(__SSE__)
+
 #define DECL_DISPATCH(ret, name, param) namespace ARCH_GENERIC { ret name param; }\
 namespace ARCH_SSE4_1 { ret name param; }\
 namespace ARCH_AVX2 { ret name param; }\
@@ -59,12 +60,18 @@ case ::SIMD::Arch::AVX2: return ARCH_AVX2::name;\
 default: return ARCH_GENERIC::name;\
 }}\
 const std::function<decltype(ARCH_GENERIC::name)> name = dispatch_target_##name();
+
 #else
+
 #define DECL_DISPATCH(ret, name, param) namespace ARCH_GENERIC { ret name param; }\
 inline std::function<decltype(ARCH_GENERIC::name)> dispatch_target_##name() {\
 return ARCH_GENERIC::name;\
 }\
 const std::function<decltype(ARCH_GENERIC::name)> name = dispatch_target_##name();
+
+#define ARCH_SSE4_1 ARCH_GENERIC
+#define ARCH_AVX2 ARCH_GENERIC
+
 #endif
 
 std::string features();
