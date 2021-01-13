f46140a499faa93ee6239a501ea5b1d20b5f1e46
Patrick Ettenhuber <pettenhuber@gmail.com>
2021-01-13 13:38:56 +0100

    merge master

--- src/cluster/disjoint_set.h
+++ src/cluster/disjoint_set.h
@@ -25,6 +25,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <unordered_set>
 #include <vector>
 
+#pragma once
+
 using namespace std;
 
 template<typename T> class Node {
