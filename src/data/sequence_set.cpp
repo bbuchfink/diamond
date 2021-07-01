ef1d77a9d2ff7198748f93371a9ab4d85a17cbd8
Benjamin Buchfink <buchfink@gmail.com>
2021-07-01 15:54:03 +0200

    Fixed compiler error.

--- src/data/sequence_set.cpp
+++ src/data/sequence_set.cpp
@@ -20,6 +20,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ****/
 
+#include <atomic>
 #include "sequence_set.h"
 #include "../util/util.h"
 #include "../util/sequence/sequence.h"
