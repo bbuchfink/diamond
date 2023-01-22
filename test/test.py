fee4c4328714ec14ab73d237c39d9bfd522eb964
Zhang.H.N <zhang.h.n@foxmail.com>
2023-01-22 20:47:39 +0800

    update test

---
+++ test/test.py
@@ -0,0 +1,14 @@
+from diamondpy import Diamond
+
+diamond = Diamond(
+    database="test/cafa4.dmnd",
+    n_threads=4,
+)
+print(diamond.version)
+diamond.dbinfo()
+diamond.blastp(
+    query="test/test_proteins.fasta",
+    out="test/test_blastp_output"
+)
+# diamond.makedb("/home/hongning/AnnoPRO/test_proteins.fasta")
+print("done")
