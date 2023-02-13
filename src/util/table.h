359b970051431890e3a114d3cb63c809f193bd1e
Benjamin Buchfink <buchfink@gmail.com>
2023-02-13 10:59:40 +0100

    Fixed compiler errors.

--- src/util/table.h
+++ src/util/table.h
@@ -32,6 +32,18 @@ struct Table {
 		return *this;
 	}
 
+	Table& operator()(const std::string& s, long n, const char* unit = "") {
+		data_.emplace_back(s, std::to_string(n) + unit);
+		max_len_ = std::max(max_len_, s.length());
+		return *this;
+	}
+
+	Table& operator()(const std::string& s, unsigned long n, const char* unit = "") {
+		data_.emplace_back(s, std::to_string(n) + unit);
+		max_len_ = std::max(max_len_, s.length());
+		return *this;
+	}
+
 	Table& operator()(const std::string& s, double n, const char* unit = "") {
 		data_.emplace_back(s, (n >= 100.0 ? std::to_string((int64_t)std::round(n)) : std::to_string(n)) + unit);
 		max_len_ = std::max(max_len_, s.length());
