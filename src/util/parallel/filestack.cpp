7b994bd0ef33c968c2f3ad20c039b68f61495ea1
Leon Adomaitis <leon@adomaitis.de>
2026-05-06 01:23:27 +0200

    fix: include <algorithm> in filestack

--- src/util/parallel/filestack.cpp
+++ src/util/parallel/filestack.cpp
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <string>
 #include <string.h>
 #include <iostream>
+#include <algorithm>
 #include <chrono>
 #include <thread>
 #include <stdexcept>
